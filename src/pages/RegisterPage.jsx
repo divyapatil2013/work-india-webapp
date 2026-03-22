9a95becffb46c2c6081b40ab51ebfe6d0f024ec1
Sweta Kadam <kadamsweta92@gmail.com>
2026-03-22 17:42:23 +0530

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -100,7 +100,7 @@ export default function RegisterPage() {
         } else {
             setLoading(true)
             try {
-                const response = await fetch('http://localhost:5001/api/auth/register', {
+                const response = await fetch('https://work-india-api.onrender.com/api/auth/register', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
